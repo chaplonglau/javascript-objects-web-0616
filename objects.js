56bac22c580565f25a330dd4f044895996813e96
chap <chaplonglau@gmail.com>
2016-07-20 11:54:23 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+
+var playlist= { bob: "bob's song"}
+
+function updatePlaylist (playlist, artist_name, song_title){
+  playlist[artist_name]=song_title;
+  return playlist; 
+}
+
+function removeFromPlaylist (playlist, artist_name){
+  delete playlist[artist_name];
+  return playlist;
+}
